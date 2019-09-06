799dd74b03d625959a30b8d16e1efd2748e37218
laptop <yuxuan0d@outlook.com>
2019-09-06 19:43:44 +0800

    和为S的两个数字

---
+++ FindNumbersWithSum.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> FindNumbersWithSum(vector<int> array, int sum)
+    {
+    }
+};
