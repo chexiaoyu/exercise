20d3779dda2754c4baefde40f7b7abae4e55e289
yx_wang <yx_wang@anngic.com>
2019-09-11 01:49:27 +0800

    删除测试文件

---
+++ uniquePaths.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m * n, vector<int>(2));
+        for (int i = 0; i < m*n; i++)
+        {
+            dp[i][0]
+        }
+    }
+};
+int main()
+{
+    Solution solution;
+
+    printf();
+    system("pause");
+    return 0;
+}
